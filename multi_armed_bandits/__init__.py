fb5bc223e015643af8cb676b0b539719433b8304
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-07-23 10:21:27 -0700

    refactor: changed std to be one

--- multi_armed_bandits/__init__.py
+++ multi_armed_bandits/__init__.py
@@ -4,6 +4,6 @@ for arms in range(2, 11):
     register(
         id=f'{arms}-Armed-Bandit',
         entry_point='multi_armed_bandits.envs:MultiArmedBanditEnv',
-        kwargs={'n_bandits': arms},
+        kwargs={'n_bandits': arms, "lowerbound_std": 1, "upperbound_std": 1},
         nondeterministic=True,
     )
