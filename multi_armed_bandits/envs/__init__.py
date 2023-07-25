6543e942901f2aca2d8dacb84951c8ac90600d68
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-07-25 07:38:59 -0700

    fix: fixed import statement in init

--- multi_armed_bandits/envs/__init__.py
+++ multi_armed_bandits/envs/__init__.py
@@ -1,2 +1,2 @@
 from multi_armed_bandits.envs.mab_env import MultiArmedBanditEnv, NormalBandit
-from multi_armed_bandits.envs.multi_armed_bandit_env import RandomContextualBandit
+from multi_armed_bandits.envs.contextual_mab_env import RandomContextualBandit
