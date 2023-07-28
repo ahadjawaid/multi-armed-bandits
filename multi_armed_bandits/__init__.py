f51586331a8f9083266941809cb585e3c9d6957e
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-07-28 15:39:36 -0700

    refactor: made register_environments function

--- multi_armed_bandits/__init__.py
+++ multi_armed_bandits/__init__.py
@@ -1,18 +1,22 @@
 from gym.envs.registration import register
 
-for n_arms in range(2, 11):
-    register(
-        id=f'{n_arms}-armed-bandit',
-        entry_point='multi_armed_bandits.envs:MultiArmedBanditEnv',
-        kwargs={'n_bandits': n_arms, "lowerbound_std": 1, "upperbound_std": 1},
-        nondeterministic=True,
-    )
-
-    for n_states in range(2, 11):
+def register_environments():
+    for n_arms in range(2, 11):
         register(
-            id=f'{n_states}-context-{n_arms}-armed-bandit',
-            entry_point='multi_armed_bandits.envs:RandomContextualBandit',
-            kwargs={'n_states': n_states, 'n_bandits': n_arms, 
-                    "lowerbound_std": 1, "upperbound_std": 1},
+            id=f'{n_arms}-armed-bandit',
+            entry_point='multi_armed_bandits.envs:MultiArmedBanditEnv',
+            kwargs={'n_bandits': n_arms, "lowerbound_std": 1, "upperbound_std": 1},
             nondeterministic=True,
         )
+
+        for n_states in range(2, 11):
+            register(
+                id=f'{n_states}-context-{n_arms}-armed-bandit',
+                entry_point='multi_armed_bandits.envs:RandomContextualBandit',
+                kwargs={'n_states': n_states, 'n_bandits': n_arms, 
+                        "lowerbound_std": 1, "upperbound_std": 1},
+                nondeterministic=True,
+            )
+
+if __name__ == "__main__":
+    register_environments()
